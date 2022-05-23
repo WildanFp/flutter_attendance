230e2fd9bbf6d1efdf1178ee7783db2638fda9b6
roberttijani <roberttijani87@gmail.com>
2022-05-23 22:24:43 +0700

    update login screen

--- lib/loginscreen.dart
+++ lib/loginscreen.dart
@@ -3,7 +3,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({ Key? key }) : super(key: key);
+  const LoginScreen({Key? key}) : super(key: key);
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
@@ -16,7 +16,7 @@ class _LoginScreenState extends State<LoginScreen> {
   double screenHeigh = 0;
   double screenWidth = 0;
 
-  Color primary = const Color(0xffeef444c);
+  Color primary = Color.fromARGB(253, 68, 176, 239);
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +24,7 @@ class _LoginScreenState extends State<LoginScreen> {
     var screenWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Column(
         children: [
           Container(
@@ -33,6 +34,7 @@ class _LoginScreenState extends State<LoginScreen> {
               color: primary,
               borderRadius: const BorderRadius.only(
                 bottomRight: Radius.circular(70),
+                bottomLeft: Radius.circular(70),
               ),
             ),
             child: Center(
@@ -40,8 +42,8 @@ class _LoginScreenState extends State<LoginScreen> {
                 Icons.person,
                 color: Colors.white,
                 size: screenWidth / 5,
-              ), 
               ),
+            ),
           ),
           Container(
             margin: EdgeInsets.only(
@@ -59,70 +61,100 @@ class _LoginScreenState extends State<LoginScreen> {
           Container(
             alignment: Alignment.centerLeft,
             margin: EdgeInsets.symmetric(
-              horizontal: screenWidth /12,
+              horizontal: screenWidth / 12,
             ),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+                fieldTitle("ID Karyawan"),
+                customField("Masukkan Id Karyawan", idController),
+                fieldTitle("Password"),
+                customField("Masukkan Password", passController),
                 Container(
-                  margin: const EdgeInsets.only(bottom: 12),
-                  child: Text(
-                  "Employee ID",
-                  style: TextStyle(
-                    fontSize: screenWidth / 26,
-                    fontFamily: "NexaBold",
-                  ),
-                ),
-                ),
-                Container(
+                  height: 60,
                   width: screenWidth,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.all(Radius.circular(12)),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black26,
-                        blurRadius: 10,
-                        offset: Offset(2, 2),
-                      )
-                    ]
+                  margin: EdgeInsets.only(top: screenWidth / 40),
+                  decoration: BoxDecoration(
+                    color: primary,
+                    borderRadius: const BorderRadius.all(Radius.circular(30)),
                   ),
-                  child: Row(
-                    children:[
-                      Container(
-                        width: screenWidth / 8,
-                        child: Icon(
-                          Icons.person,
-                          color: primary,
-                          size: screenWidth / 15,
-                        ),
+                  child: Center(
+                    child: Text(
+                      "LOGIN",
+                      style: TextStyle(
+                        fontFamily: "NexaBold",
+                        fontSize: screenWidth / 26,
+                        color: Colors.white,
+                        letterSpacing: 2,
                       ),
-                      Expanded(
-                        child: Padding(
-                          padding: EdgeInsets.only(right: screenWidth /12),
-                          child: TextFormField(
-                            enableSuggestions: false,
-                            autocorrect: false,
-                            decoration: InputDecoration(
-                              contentPadding: EdgeInsets.symmetric(
-                                vertical: screenHeight / 35,
-                              ),
-                              border: InputBorder.none,
-                              hintText: "Enter your employee id",
-                            ),
-                          ),
-                        ),
-                      )
-                    ],
-                  )
-                )
+                    ),
+                  ),
+                ),
               ],
             ),
-          )
-        ]
+          ),
+        ],
       ),
     );
   }
 
+  Widget fieldTitle(String title) {
+    return Container(
+      margin: const EdgeInsets.only(bottom: 12),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontSize: screenWidth / 26,
+          fontFamily: "NexaBold",
+        ),
+      ),
+    );
+  }
 
+  Widget customField(String hint, TextEditingController controller) {
+    return Container(
+      width: screenWidth,
+      margin: EdgeInsets.only(bottom: 12),
+      decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black26,
+              blurRadius: 10,
+              offset: Offset(2, 2),
+            )
+          ]),
+      child: Row(
+        children: [
+          Container(
+            width: screenWidth / 8,
+            child: Icon(
+              Icons.person,
+              color: primary,
+              size: screenWidth / 15,
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.only(right: screenWidth / 12),
+              child: TextFormField(
+                controller: controller,
+                enableSuggestions: false,
+                autocorrect: false,
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(
+                    vertical: screenHeigh / 35,
+                  ),
+                  border: InputBorder.none,
+                  hintText: hint,
+                ),
+                maxLines: 1,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 }
