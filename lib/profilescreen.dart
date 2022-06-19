1880ecc6c548ac3b403f03ab936359cbf5271921
roberttijani <roberttijani87@gmail.com>
2022-06-19 17:57:16 +0700

    update profil

--- lib/profilescreen.dart
+++ lib/profilescreen.dart
@@ -1,20 +1,121 @@
 import 'package:flutter/material.dart';
 
+import 'model/user.dart';
+
 class ProfileScreen extends StatefulWidget {
-  const ProfileScreen({ Key? key }) : super(key: key);
+  const ProfileScreen({Key? key}) : super(key: key);
 
   @override
   State<ProfileScreen> createState() => _ProfileScreenState();
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  double screenHeight = 0;
+  double screenWidth = 0;
+  Color primary = Color.fromARGB(253, 68, 176, 239);
+
   @override
   Widget build(BuildContext context) {
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: Center(
-        child: Text("Profile"),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Container(
+              margin: const EdgeInsets.only(top: 20, bottom: 20),
+              height: 120,
+              width: 120,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(100),
+                color: primary,
+              ),
+              child: const Center(
+                child: Icon(
+                  Icons.person,
+                  size: 80,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.center,
+              child: Text(
+                "Karyawan ${User.idkaryawan}",
+                style: const TextStyle(
+                  fontFamily: "NexaBold",
+                  fontSize: 18,
+                ),
+              ),
+            ),
+            const SizedBox(height: 10),
+            textField("First Name", "First Name"),
+            textField("Last Name", "Last Name"),
+            Container(
+              height: kToolbarHeight,
+              width: screenWidth,
+              margin: const EdgeInsets.only(bottom: 12),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(4),
+                border: Border.all(color: Colors.black54),
+              ),
+              child: Container(
+                padding: const EdgeInsets.only(left: 11),
+                alignment: Alignment.centerLeft,
+                child: const Text(
+                  "Date of birth",
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontFamily: "NexaBold",
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+            textField("Address", "Address"),
+          ],
+        ),
       ),
-      
+    );
+  }
+
+  Widget textField(String tittle, String hint) {
+    return Column(
+      children: [
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Text(
+            tittle,
+            style:
+                const TextStyle(fontFamily: "NexaBold", color: Colors.black87),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.only(bottom: 12),
+          child: TextFormField(
+            cursorColor: Colors.black54,
+            maxLines: 1,
+            decoration: InputDecoration(
+              hintText: hint,
+              hintStyle: const TextStyle(
+                color: Colors.black54,
+                fontFamily: "NexaBold",
+              ),
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.black54,
+                  width: 1,
+                ),
+              ),
+              focusedBorder: const OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black54),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
